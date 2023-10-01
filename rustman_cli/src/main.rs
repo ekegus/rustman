8ecf2bf72cef1a153b01edc7f0c41d1badab0c55
Ben Busch <ben.busch@breedr.co>
2023-10-01 21:14:47 +0100

    Simplify logic in main function

--- rustman_cli/src/main.rs
+++ rustman_cli/src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::{anyhow, Result};
+use anyhow::anyhow;
 use rustman_api::{fetch_data, parse_word_from_json};
 use rustman_game::{Game, GameOutcome, GameState};
 use std::io::{self, Write};
@@ -6,7 +6,7 @@ use std::io::{self, Write};
 const MAX_ATTEMPTS: i8 = 5;
 const URL: &str = "https:random-word-api.herokuapp.com/word";
 
-fn get_secret_word() -> Result<String> {
+fn get_secret_word() -> anyhow::Result<String> {
     let data = fetch_data(URL).map_err(|error| anyhow!("Failed to make API call: {}", error))?;
 
     let secret_word =
@@ -15,7 +15,7 @@ fn get_secret_word() -> Result<String> {
     Ok(secret_word)
 }
 
-fn get_user_input(user_instruction: &str) -> Result<String, io::Error> {
+fn get_user_input(user_instruction: &str) -> anyhow::Result<String> {
     println!("{}", user_instruction);
 
     let mut user_input = String::from("");
@@ -27,7 +27,7 @@ fn get_user_input(user_instruction: &str) -> Result<String, io::Error> {
     Ok(user_input.trim().to_string())
 }
 
-fn play_game(mut game: Game) -> Result<Game, io::Error> {
+fn play_game(mut game: Game) -> anyhow::Result<()> {
     while *game.get_game_state() == GameState::InProgress {
         println!("Attempts left: {}", game.attempts_left);
         println!("Word: {}", game.get_display_word());
@@ -47,33 +47,38 @@ fn play_game(mut game: Game) -> Result<Game, io::Error> {
         }
     }
 
-    return Ok(game);
+    let game_outcome = game.evaluate_game_outcome();
+
+    match game_outcome {
+        GameOutcome::Winner(secret_word) => {
+            println!("Congratulations! You guessed the word: {}", secret_word)
+        }
+        GameOutcome::Loser(secret_word) => {
+            println!(
+                "Sorry, you ran out of attempts. The word was: {}",
+                secret_word
+            )
+        }
+        GameOutcome::InProgress => (),
+    }
+    return Ok(());
 }
 
-fn main() -> Result<(), io::Error> {
-    match get_secret_word() {
-        Ok(secret_word) => {
-            let game = Game::new(secret_word, MAX_ATTEMPTS);
+fn terminate_with_error(error: anyhow::Error) -> ! {
+    eprintln!("{}", error);
+    std::process::exit(1);
+}
 
-            let mut game = play_game(game)?;
-
-            let game_outcome = game.evaluate_game_outcome();
-
-            match game_outcome {
-                GameOutcome::Winner(secret_word) => {
-                    println!("Congratulations! You guessed the word: {}", secret_word)
-                }
-                GameOutcome::Loser(secret_word) => {
-                    println!(
-                        "Sorry, you ran out of attempts. The word was: {}",
-                        secret_word
-                    )
-                }
-                GameOutcome::InProgress => (),
-            }
-        }
-        Err(error) => eprintln!("{}", error),
-    }
+fn run() -> anyhow::Result<()> {
+    let secret_word = get_secret_word()?;
+    let game = Game::new(secret_word, MAX_ATTEMPTS);
+    play_game(game)?;
 
     Ok(())
+}
+
+fn main() {
+    if let Err(error) = run() {
+        terminate_with_error(error);
+    }
 }
